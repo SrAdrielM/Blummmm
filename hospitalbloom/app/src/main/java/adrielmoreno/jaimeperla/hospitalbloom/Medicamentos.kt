6f5d5dbd8fb1c2dd481ceaff3b82cc08d08a622a
SrAdrielM <20230542@ricaldone.edu.sv>
2024-07-23 22:03:14 -0600

    formulario de agregar papus

--- hospitalbloom/app/src/main/java/adrielmoreno/jaimeperla/hospitalbloom/Medicamentos.kt
+++ hospitalbloom/app/src/main/java/adrielmoreno/jaimeperla/hospitalbloom/Medicamentos.kt
@@ -34,7 +34,7 @@ class Medicamentos : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_medicamentos, container, false)
+        return inflater.inflate(R.layout.fragment_addpaciente, container, false)
     }
 
     companion object {
