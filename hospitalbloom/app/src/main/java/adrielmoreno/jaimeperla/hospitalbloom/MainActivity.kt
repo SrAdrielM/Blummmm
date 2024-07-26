9b2a2063f11a480701d58a59e7b448515946d7e7
SrAdrielM <20230542@ricaldone.edu.sv>
2024-07-25 19:57:57 -0600

    no pude arreglar nada shitttttttt

--- hospitalbloom/app/src/main/java/adrielmoreno/jaimeperla/hospitalbloom/MainActivity.kt
+++ hospitalbloom/app/src/main/java/adrielmoreno/jaimeperla/hospitalbloom/MainActivity.kt
@@ -1,10 +1,7 @@
 package adrielmoreno.jaimeperla.hospitalbloom
 
 import adrielmoreno.jaimeperla.hospitalbloom.databinding.ActivityMainBinding
-import android.app.Activity
 import android.os.Bundle
-import android.view.inputmethod.InputBinding
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -18,23 +15,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
-
-        enableEdgeToEdge()
         setContentView(binding.root)
         replaceFragment(Pacientes())
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
         binding.bottomNav.setOnItemSelectedListener {
             when(it.itemId){
                 R.id.icPacientes -> replaceFragment(Pacientes())
-                R.id.icMedicamentos -> replaceFragment(Medicamentos())
-
+                R.id.icMedicamentos -> replaceFragment(addpaciente())
                 else -> {
-
                 }
             }
             true
@@ -42,9 +30,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun replaceFragment(fragment: Fragment) {
-        val fragmenttManager = supportFragmentManager
-        val fragmentTransaction = fragmenttManager.beginTransaction()
-        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
         fragmentTransaction.commit()
     }
 }
